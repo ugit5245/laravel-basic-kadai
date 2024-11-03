2c7b08f9eb49bf2d73d6d953a9e859fd8e89a100
ugit5245 <umex.orque2@gmail.com>
2024-11-03 15:59:18 +0900

    kadai19_pre_htdocs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/posts',[PostController::class, 'index']);
+
+Route::get('/posts/{id}',[PostController::class, 'show']);
