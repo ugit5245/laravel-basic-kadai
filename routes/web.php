d96bcfe0c9c412d79a50fcd70ac2a0c9856f4adc
ugit5245 <umex.orque2@gmail.com>
2024-11-08 01:16:56 +0900

    kadai32pre

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PostController;
-
-
 
 
 /*
@@ -22,12 +20,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/posts', [PostController::class, 'index']);
 
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
 
-Route::get('/posts/create', [PostController::class, 'create']);
-
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
 
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
